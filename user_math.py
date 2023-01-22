dfda59e8389068b0831e52d39d1e127b153abe08
Eric.Meyer <eric.meyer2019@gmail.com>
2023-01-22 12:56:13 -0600

    Math and functions practice / output

--- user_math.py
+++ user_math.py
@@ -1,6 +1,7 @@
 """
-Always start with a docstring that describes what the module does.
-Include your name and the date.
+
+Eric Meyer - 1/21/2023
+Learning the math module in this python file. 
 
 Use built-in functions and 
 functions from the math module.
@@ -16,7 +17,7 @@ import math
 
 # define some functions
 
-def get_area_of_lot(length, width):
+def get_area_of_lot(length, width):  # get_area_of_lot function takes 2 arguments (length and width)
     """
     Return area of a lot given the length and width of the lot.
 
@@ -27,7 +28,7 @@ def get_area_of_lot(length, width):
     # Use a try / except / finally block when something 
     # could go wrong
     try: 
-        area = 0 # fix this
+        area = length * width # fix this -- lenth and width (arguments for defined function) were not used in the area equation
         return area
     except Exception as ex:
         print(f"Error: {ex}")
@@ -36,8 +37,72 @@ def get_area_of_lot(length, width):
 
 # define more functions here (see instuctions)
 
+## Prorated employee PTO calculator function (custom function 1):
 
+def prorated_pto_balance(annual_accrual, start_month):
+    """
+    Calculate an employee's prorated PTO balance for the year based off what month they start during the
+    course the calendar year.
 
+    Argument considerations:
+        - How many PTO hours do employees receive annaully based off years of experience? (1-2 years of experience = 80 hours pto, 3-5 years of experience = 120 hours pto, etc.)
+        - Start month is straight forward (1-3 = january through march, 4-6 = april through june, 7-9 = july through september, and 10-12 = october through december)
+
+    """
+
+    if start_month <= 3:
+            start_month == float(1)
+    elif start_month >= 4 and start_month <= 6:
+            start_month = float(0.75)
+    elif start_month >= 7 and start_month <= 9:
+            start_month = float(0.5)
+    elif start_month >= 10 and start_month <= 12:
+            start_month = float(0.25)
+    
+    try:
+        prorated_pto = start_month * annual_accrual
+        return prorated_pto
+    except Exception as ex:
+        print(f"Error: {ex}")
+        return None
+    
+## Employee time off totaling calculator (custom function 2):
+
+def total_employee_time_off_work(hours_off1, hours_off2, hours_off3, hours_off4):
+    """
+    Simply adding up an employee's hours over the course of a year in order to calculate
+    the employee's total hours off work.
+
+    hours_off should be a floating number!
+
+    """
+
+    time_off_list = [hours_off1, hours_off2, hours_off3, hours_off4]
+
+    try:
+        total_time_off = math.fsum(time_off_list) # used math module's fsum function
+        return total_time_off
+    except Exception as ex:
+        print(f"Error: {ex}")
+        return None
+    
+## Employee weekend pay payroll calculator (custom function 3):
+
+def weekend_prem_payroll(base_payroll, weekend_nights_out):
+    """
+    Adding premium pay to an employee's payroll. Simply adding $150 per weekend night out
+    with this payroll function.
+
+    """
+
+    weekend_premium = weekend_nights_out * 150   # $150 for each weekend night out for work
+
+    try:
+        total_payroll = base_payroll + weekend_premium
+        return total_payroll
+    except Exception as ex:
+        print(f"Error: {ex}")
+        return None
 
 # -------------------------------------------------------------
 # Call some functions and execute code!
@@ -49,4 +114,28 @@ def get_area_of_lot(length, width):
 if __name__ == "__main__":
 
     # call your functions here (see instructions)
-    print("your code here")
+    print('Exploring the math module by defining functions!')
+    print()
+    print(f"math.comb(10,1) = {math.comb(10,1)}") # testing various number combos in order to grasp the combination and permuatation math functions
+    print(f"math.perm(10,1) = {math.perm(10,1)}")
+    print()
+    print(f"math.comb(10,2) = {math.comb(10,2)}")
+    print(f"math.perm(10,2) = {math.perm(10,2)}")
+    print()
+    print(f"get_area_of_lot(30,15) = {get_area_of_lot(30,15)}")
+    print(f"get_area_of_lot(30,0) = {get_area_of_lot(30,0)}")
+    print(f"get_area_of_lot(0,15) = {get_area_of_lot(0,15)}")
+    print(f"get_area_of_lot(6,2) = {get_area_of_lot(6,2)}")
+    print()
+    print(f"prorated_pto_balance(80,5) = {prorated_pto_balance(80,5)}")
+    print(f"prorated_pto_balance(80,1) = {prorated_pto_balance(80,1)}")
+    print(f"prorated_pto_balance(80,8) = {prorated_pto_balance(80,8)}")
+    print(f"prorated_pto_balance(80,11) = {prorated_pto_balance(80,11)}")
+    print()
+    print(f"total_employee_time_off_work(32,8,40,4.5) = {total_employee_time_off_work(32,8,40,4.5)}")
+    print(f"total_employee_time_off_work(1,2,3,4) = {total_employee_time_off_work(1,2,3,4)}")
+    print(f"total_employee_time_off_work(80,8,8,16) = {total_employee_time_off_work(80,8,8,16)}")
+    print()
+    print(f"weekend_prem_payroll(2200,3) = {weekend_prem_payroll(2200,3)}")
+    print(f"weekend_prem_payroll(4000,1) = {weekend_prem_payroll(4000,1)}")
+    print(f"weekend_prem_payroll(3200,0) = {weekend_prem_payroll(3000,0)}")
---
+++ user_mathout.py
@@ -0,0 +1,31 @@
+"""
+user_math output below!!
+________________________
+
+Exploring the math module by defining functions!
+
+math.comb(10,1) = 10
+math.perm(10,1) = 10
+
+math.comb(10,2) = 45
+math.perm(10,2) = 90
+
+get_area_of_lot(30,15) = 450
+get_area_of_lot(30,0) = 0
+get_area_of_lot(0,15) = 0
+get_area_of_lot(6,2) = 12
+
+prorated_pto_balance(80,5) = 60.0
+prorated_pto_balance(80,1) = 80
+prorated_pto_balance(80,8) = 40.0
+prorated_pto_balance(80,11) = 20.0
+
+total_employee_time_off_work(32,8,40,4.5) = 84.5
+total_employee_time_off_work(1,2,3,4) = 10.0
+total_employee_time_off_work(80,8,8,16) = 112.0
+
+weekend_prem_payroll(2200,3) = 2650
+weekend_prem_payroll(4000,1) = 4150
+weekend_prem_payroll(3200,0) = 3000
+
+"""
