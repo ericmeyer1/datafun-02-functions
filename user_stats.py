c7f69b7de68b54b3f9fc34cdf118af4983682424
Eric.Meyer <eric.meyer2019@gmail.com>
2023-01-22 14:04:23 -0600

    Stats practice and results

--- user_stats.py
+++ user_stats.py
@@ -1,4 +1,6 @@
 """
+Eric Meyer
+1/22/23
 
 This example illustrates basic analytics
 using just the built-in statistics module.
@@ -73,8 +75,55 @@ scores = [
     104,
 ]
 
+# Descriptive: measures of cental tendency
+
+mean = statistics.mean(scores)
+median = statistics.median(scores)
+mode = statistics.mode(scores)
+
+# Descriptive: measures of spread
+
+var = statistics.variance(scores)
+stdev = statistics.stdev(scores)
+
+print()
+print("=============================================================")
+print()
+print("Descriptive statistics include measures of central tendancy:")
+print(f"   mean = {mean:.2f}")
+print(f"   median = {median:.2f}")
+print(f"   mode = {mode:.2f}")
+print()
+print("Descriptive statistics include measures of spread:")
+print(f"   var = {var:.2f}")
+print(f"   stddev = {stdev:.2f}")
+print()
+
 # univariant time series data (one varabile over time)
 # typically, x (or time) is independent and
 # y is dependent on x (e.g. temperature vs hour of day)
+
 x_times = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-y_temps = [2, 5, 8, 20, 21, 23, 24, 27, 30, 31, 31,32]
+y_temps = [2, 5, 8, 20, 21, 23, 24, 27, 30, 31, 31, 32]
+
+slope, intercept = statistics.linear_regression(x_times, y_temps)
+
+future_time = 13
+
+future_temp = round(slope * future_time + intercept)
+
+print("=============================================================")
+print()
+print("Calculate the slope and intercept of a best fit straight line:")
+print()
+print(f"slope = {slope:.2f}")
+print(f"intercept = {intercept:.2f}")
+print()
+print("Let's use our best fit line to PREDICT a future temperature.")
+print()
+print(f"At the future time of hour {future_time:d},")
+print(f"we predict the future temp will be {future_temp:d} degrees Fahrenheit.")
+print()
+print("How'd we do? Does this make sense given the data?")
+print(input("User response to results of first ever ML example: "))
+print()
---
+++ user_statsout.py
@@ -0,0 +1,28 @@
+"""
+
+Descriptive statistics include measures of central tendancy:
+   mean = 99.12
+   median = 102.50
+   mode = 111.00
+
+Descriptive statistics include measures of spread:
+   var = 179.25
+   stddev = 13.39
+
+=============================================================
+
+Calculate the slope and intercept of a best fit straight line:
+
+slope = 2.78
+intercept = 3.12
+
+Let's use our best fit line to PREDICT a future temperature.
+
+At the future time of hour 13,
+we predict the future temp will be 39 degrees Fahrenheit.
+
+How'd we do? Does this make sense given the data?
+User response to results of first ever ML example: BOOM! GOT IT!
+BOOM! GOT IT!
+
+"""
