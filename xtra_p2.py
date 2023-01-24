cd256333f48c65a6aa1d06fc693210fe4ee0097d
Eric.Meyer <eric.meyer2019@gmail.com>
2023-01-24 10:01:19 -0600

    Bonus code  & results

--- xtra_p2.py
+++ xtra_p2.py
@@ -1,7 +1,12 @@
 """
-Optional bonus. See course site for details.
+Eric Meyer
 
+Date Completed: 1/24/2023
 
+Northwest MS-DA couse Data Analytics Fundamentals - Module 2 - Project 2 - Task 7 (Bonus)
+
+Testing for understanding of functions in the xtra problem for module 2 project. Also utilizing doctest to be able to instantly test if you code is working
+without having to write out print functions for each function (great and handy for large projects).
 
 >>> add_two(1,2)
 3
@@ -28,9 +33,11 @@ Optional bonus. See course site for details.
 import doctest
 
 # define some existing functions
+
+
 def add_two(first, second):
     """Return the sum of any two arguments."""
-    sum = first   # fix this line
+    sum = first + second  # fix this line -- DONE
     return sum
 
 
@@ -38,7 +45,7 @@ def add_triangle_list(list_triangle):
     """Return the sum of three numbers in a list."""
     sum = 0
     for value in list_triangle:
-        sum = sum + 0  # fix this line to add the value instead of 0
+        sum = sum + value  # fix this line to add the value instead of 0 -- DONE
     return sum
 
 
@@ -46,7 +53,7 @@ def add_any(*args):
     """Return the sum of numbers, using built-in *args."""
     sum = 0
     for x in args:
-        sum += 1  # fix this line to add x instead of 1
+        sum += x  # fix this line to add x instead of 1 -- DONE
     return sum
 
 
@@ -54,41 +61,26 @@ def add_any_with_keywords(**kwargs):
     """Return the sum of numbers, using built-in keyword args, **kwargs."""
     sum = 0
     for value in kwargs.values():  # use values() - name doesn't matter
-        sum += value  # Use the popular and concise version of sum = sum + x
+        sum += value  # Use the popular and concise version of sum = sum + x -- DONE
     return sum
-
 
 # TODO: implment a new function to convert celsius to fahrenheit
 # Use round as needed to make the test pass
 # The name of the function is provided in the docstring above
 
 
-
-
-
-
-
-
+def convert_ctof(celsius_value):
+    """Return the fahrenheit value of a entered celsuis value."""
+    fahrenheit_value = int(celsius_value) * (9/5) + 32
+    return fahrenheit_value
 
 
 if __name__ == "__main__":
 
-
-    print("===========================================================")
-    print("Running doctest.testmod() function to unit test our code")
-    print("===========================================================")
+    doctest.testmod()  # no messages show in terminal meaning doctest was showing that code was correct...I then added code to test convert_ctof
     print()
-    doctest.testmod()
+    # testing results visually in terminal as well! The celsuis to fahrenheit converter works!
+    print(f"convert_ctof(0) = {round((convert_ctof(0)),2)}")
+    # also set up round function to clean up any results to within two decimal points
+    print(f"convert_ctof(100) = {round((convert_ctof(108.55)),2)}")
     print()
-    print("===========================================================")
-    print("If you don't see any results, all tests passed.")
-    print("===========================================================")
-    print("Run with the -v flag to show results regardless.")
-    print("Hit up arrow (to get last command) and add space -v")
-    print("===========================================================")
-    print()
-    print("Read error messages carefully.")
-    print("Errors tell which line number has the error")
-    print("and what the issue is. Scroll up in the terminal to see.")
-    print("Fix issues one at a time until you get the behavior")
-    print("Described in the docstring.")
---
+++ xtra_p2out.py
@@ -0,0 +1,11 @@
+"""
+
+xtra_p2 file OUTPUT!
+_________________________
+
+
+convert_ctof(0) = 32.0
+convert_ctof(100) = 226.4
+
+
+"""
